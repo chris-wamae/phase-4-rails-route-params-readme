e3efbfda523fb6adc7dc085ce5d6d707f8918bba
chris-wamae <chriswamae123@gmail.com>
2023-03-13 09:33:52 +0300

    Completed Assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,9 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    single_cheese = Cheese.find(params[:id])
+    render json: single_cheese
+  end
+
 end
